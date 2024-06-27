e5a6c3ab87138d7d918d5731d5eb13f863c1b918
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-06-27 23:09:04 +0530

    init

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -18,6 +18,9 @@ export default function Home() {
           <li>
             Get started by editing <code>app/page.tsx</code>
           </li>
+          <li>
+            This is doc
+          </li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
